74d9a08b02d251ba35a8920be5f93023a5ba3317
Taylor Hershberger <31741088+TaylorDay11@users.noreply.github.com>
2024-10-18 14:39:13 -0600

    Update Card.js
    fixed open link in new tab

--- components/Card.js
+++ components/Card.js
@@ -11,7 +11,7 @@ export default function Card(props){
                     <div className="location-details">
                         <img className="map-marker" src="../img/MapMarker.svg" loading="lazy" />
                         <h3 className="state">{props.location}</h3>
-                        <a className="google-map-link" href={`${props.googleMapsUrl}`} targte="_blank">View on Google Maps</a>
+                        <a className="google-map-link" href={`${props.googleMapsUrl}`} target="_blank">View on Google Maps</a>
                     </div>
                     <h1 class="title">{props.title}</h1>
                     <p className="date">{props.date}</p>
