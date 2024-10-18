b4cbfa82975c4c0848d78c6e86b40c724a55bc40
Taylor Hershberger <31741088+TaylorDay11@users.noreply.github.com>
2024-10-18 14:29:32 -0600

    upload files

---
+++ components/Card.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Card(props){
+    return(
+        <div className="card-container">
+            <div className="card">
+                <div className="left">
+                    <img className="card-img" src={`../img/${props.imageUrl}`} loading="lazy" />
+                </div>
+                <div className="right">
+                    <div className="location-details">
+                        <img className="map-marker" src="../img/MapMarker.svg" loading="lazy" />
+                        <h3 className="state">{props.location}</h3>
+                        <a className="google-map-link" href={`${props.googleMapsUrl}`} targte="_blank">View on Google Maps</a>
+                    </div>
+                    <h1 class="title">{props.title}</h1>
+                    <p className="date">{props.date}</p>
+                    <p className="description">{props.description}</p>
+                </div>
+            </div>
+        <hr />
+        </div>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar(){
+    return (
+        <nav className="navbar">
+            <img src="../img/Globe.svg" className="logo" />
+            <h2>national park travel journal.</h2>
+        </nav>
+    )
+}
